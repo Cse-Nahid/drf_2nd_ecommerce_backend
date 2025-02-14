9dbb1bfc6cc14ee6e8d17906175822493fb222ef
Cse-Nahid <princenahidcse1@gmail.com>
2025-02-14 16:16:51 +0600

    url add

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -71,6 +71,7 @@ MIDDLEWARE = [
 
     "corsheaders.middleware.CorsMiddleware",
     'allauth.account.middleware.AccountMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware", 
 ]
 
 ROOT_URLCONF = 'ecommerce.urls'
@@ -152,7 +153,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
